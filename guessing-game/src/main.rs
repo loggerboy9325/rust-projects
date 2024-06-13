410f3e78818b616d51d586197923836f4cd10279
loggerboy <loggerboy9325@gmail.com>
2024-06-13 00:05:24 -0400

    guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,25 +1,27 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    let colors = ["red", "green", "blue"];
-    let time = std::time::UNIX_EPOCH
-        .elapsed()
-        .expect("Call the doctor, time went backwards!")
-        .as_millis() as usize;
+    println!("Guess the number!");
+    println!("Please input your guess.");
 
-    let actual =  colors[time % colors.len()];
-    println!("Welcome to the guessing game!");
-    println!("I have chosen a color: red, green or blue, can you guess which?");
-    println!("Enter your guess: red, green or blue");
-    let input = std::io::stdin()
-        .lines()
-        .next()
-        .expect("No input was read")
-        .expect("There was an error when reading the input");
-    println!("Your guess was {input}");
-    println!("The color I chose was {actual}");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    if input == actual {
-        println!("You win!");
-    } else {
-        println!("You lose!");
-    }
-}
+    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number);
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32= guess.trim().parse().expect("Please type a number!");
+
+   println!("You guess: {}", guess);
+
+   match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+   }
+} 
