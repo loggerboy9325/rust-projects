95ce2df62680e5e413248ea9bda4a3fd73cdfd1b
loggerboy <loggerboy9325@gmail.com>
2024-06-14 11:31:46 -0400

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 5;
+    println!("The calue of x is: {}",x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    const NUMBER_TEST: u32 = 10000;
+
+    println!("The value of number is {}", NUMBER_TEST);
+
+
+}
